5a677b6f58daea6f6357d2fa697851e07db4f748
unknownroot0 <110045506+unknownroot0@users.noreply.github.com>
2023-01-05 00:11:19 +0600

    Update task0xE.c

--- task0xE.c
+++ task0xE.c
@@ -1,31 +1,23 @@
 // Write a c program to convert days to years, weeks and months
 
 
-#include <stdio.h>
 
-#define DAYS_IN_YEAR 365  // define a constant for the number of days in a year
-#define DAYS_IN_MONTH 30  // define a constant for the number of days in a month
+#include <stdio.h> 
 
 int main()
 {
-    int days;  // declare a variable to store the number of days
-    int years;  // declare a variable to store the number of years
-    int weeks;  // declare a variable to store the number of weeks
-    int months;  // declare a variable to store the number of months
+    int days, years, weeks;
 
-    printf("Enter the number of days: ");
-    scanf("%d", &days);  // read the number of days from the user and store it in the days variable
+    days = 1329; 
 
-    // calculate the number of years
-    years = days / DAYS_IN_YEAR;
+    // Converts days to years, weeks and days
+    years = days/365; 
+    weeks = (days % 365)/7;
+    days = days- ((years*365) + (weeks*7));
 
-    // calculate the number of weeks
-    weeks = (days % DAYS_IN_YEAR) / 7;
-
-    // calculate the number of months
-    months = ((days % DAYS_IN_YEAR) % 7) / DAYS_IN_MONTH;
-
-    printf("%d days = %d years, %d weeks, %d months\n", days, years, weeks, months);
+    printf("Years: %d\n", years);
+    printf("Weeks: %d\n", weeks);
+    printf("Days: %d \n", days);
 
     return 0;
 }
