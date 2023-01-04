62f48cff1e0df92d6ed9a2669e411aea53df0a37
unknownroot0 <110045506+unknownroot0@users.noreply.github.com>
2023-01-05 00:12:57 +0600

    Update task0xE.c

--- task0xE.c
+++ task0xE.c
@@ -7,17 +7,16 @@
 int main()
 {
     int days, years, weeks;
-
-    days = 1329; 
-
-    // Converts days to years, weeks and days
-    years = days/365; 
-    weeks = (days % 365)/7;
-    days = days- ((years*365) + (weeks*7));
-
-    printf("Years: %d\n", years);
-    printf("Weeks: %d\n", weeks);
-    printf("Days: %d \n", days);
-
+    //Ask user to input number of days
+    printf("Enter days: ");
+    scanf("%d", &days);
+    // Ignoring leap year
+    years = (days / 365);
+    weeks = (days % 365) / 7;
+    days  = (days % 365) %7;
+    //Print the result
+    printf("YEARS: %d\n", years);
+    printf("WEEKS: %d\n", weeks);
+    printf("DAYS: %d", days);
     return 0;
 }
