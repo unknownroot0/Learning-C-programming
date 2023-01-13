2b673a4cacac24702004964a99e9527122adae2a
unknownroot0 <110045506+unknownroot0@users.noreply.github.com>
2023-01-13 22:16:42 +0600

    Update task0xF.c

--- task0xF.c
+++ task0xF.c
@@ -1,1 +1,46 @@
 /* Write a C program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.
+
+
+#include <stdio.h>
+ 
+int main()
+{
+    int num1, num2, num3;
+ 
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+ 
+    if (num1 >= num2)
+    {
+        if (num1 >= num3)
+            printf("Largest number = %d", num1);
+        else
+            printf("Largest number = %d", num3);
+    }
+    else
+    {
+        if (num2 >= num3)
+            printf("Largest number = %d", num2);
+        else
+            printf("Largest number = %d", num3);
+    }
+ 
+    if (num1 <= num2)
+    {
+        if (num1 <= num3)
+            printf("\nSmallest number = %d", num1);
+        else
+            printf("\nSmallest number = %d", num3);
+    }
+    else
+    {
+        if (num2 <= num3)
+            printf("\nSmallest number = %d", num2);
+        else
+            printf("\nSmallest number = %d", num3);
+    }
+ 
+    printf("\nLarger number = %d\n", (num1 + num2 + num3) - (num1 + num2 + num3) / 3);
+ 
+    return 0;
+}
