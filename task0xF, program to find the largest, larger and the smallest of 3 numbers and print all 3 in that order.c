91310d32b82eb9bcc5fbc5a7a2a9a7bc0a8fd755
unknownroot0 <110045506+unknownroot0@users.noreply.github.com>
2023-01-14 17:05:17 +0600

    Update and rename task0xF, Write a C program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.c to task0xF, program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.c

--- task0xF, program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.c
+++ task0xF, program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.c
@@ -1,4 +1,4 @@
-/* Write a C program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.
+// Write a C program to find the largest, larger and the smallest of 3 numbers and print all 3 in that order.
 
 
 
