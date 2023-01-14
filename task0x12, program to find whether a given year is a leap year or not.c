92c622fecb25a1197990f8ccb2b9aa59a920ef1d
unknownroot0 <110045506+unknownroot0@users.noreply.github.com>
2023-01-14 17:17:48 +0600

    Update and rename task0x12.c to task0x12, program to find whether a given year is a leap year or not.c

---
+++ task0x12, program to find whether a given year is a leap year or not.c
@@ -0,0 +1,23 @@
+/* Write a C program to find whether a given year is a leap year or not.
+
+
+#include <stdio.h>
+
+int main()
+{
+  int yr;
+ 
+  printf("\n Please Enter a year \n"); 
+  scanf("%d", &yr);
+ 
+  if ( yr%400 == 0) // Check if the year is divisible by 400
+     printf("\n%d is a Leap Year. \n", yr); // If true, then it's a leap year
+  else if ( yr%100 == 0) // Check if the year is divisible by 100 
+     printf("\n%d is not. \n", yr); // If true, then it's not a leap year
+  else if ( yr%4 == 0 ) // If true, then it's not a leap year
+     printf("\n%d is a Leap Year. \n", yr); // Check if the year is divisible by 4
+  else
+     printf("\n%d is not. \n", yr);  
+ 
+  return 0;
+}
